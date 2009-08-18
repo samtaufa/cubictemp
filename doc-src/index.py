bc132e259cf901077595d96b3942008a84f095f9
Samiuela LV Taufa <samt@employersmutual.com.au>
2009-08-18 14:05:51 +1000

    countershape.grok.grok --> countershape.grok.parse

--- doc-src/index.py
+++ doc-src/index.py
@@ -1,19 +1,25 @@
-
+import os, os.path, subprocess
 import countershape.widgets
 import countershape.layout
 import countershape.grok
 from countershape.doc import *
 
+this.layout = countershape.layout.Layout("_layout.html")
+this.markdown = "rst"
+
 ns.docTitle = "Cubictemp Manual"
 ns.docMaintainer = "Aldo Cortesi"
-ns.docMaintainerEmail = "aldo@nullcube.com"
-ns.foot = "Copyright Nullcube 2008"
-ns.head = readFrom("_header.html")
-ns.sidebar = countershape.widgets.SiblingPageIndex('/index.html', exclude=['countershape'])
-this.layout = countershape.layout.TwoPane("yui-t2", "doc3")
+ns.docMaintainerEmail = "dev@nullcube.com"
+ns.copyright = "Copyright Nullcube 2008"
+ns.head = countershape.template.File(None, "_header.html")
+ns.sidebar = countershape.widgets.SiblingPageIndex(
+                '/index.html',
+                exclude=['countershape']
+            )
+
 this.titlePrefix = "Cubictemp Manual - "
 
-ns.ctgrok = countershape.grok.grok("../cubictemp.py")
+ns.ctgrok = countershape.grok.parse("../cubictemp.py")
 
 pages = [
     Page("index.html", "Introduction"),
