03dbb65afe8aa2adc9af6928d502da226bfa3311
Samiuela LV Taufa <samtaufa@gmail.com>
2011-06-15 09:42:15 +1000

    Rename *.mdtext, *.html files to *.md so GitHub can auto-render part of the documentation.

--- doc-src/index.py
+++ doc-src/index.py
@@ -18,16 +18,16 @@ this.titlePrefix = "Cubictemp Manual - "
 this.layout = countershape.layout.Layout("_layout.html")
 
 pages = [
-    Page("index.mdtext", "Overview"),
-    Page("introduction.mdtext", "Introduction"),    
-    Page("templates.mdtext", "Templates"),    
-    Page("extensions.mdtext", "Extensions"),    
-    Page("tipstricks.mdtext", "Tips & Tricks"),    
+    Page("index.md", "Overview"),
+    Page("introduction.md", "Introduction"),    
+    Page("templates.md", "Templates"),    
+    Page("extensions.md", "Extensions"),    
+    Page("tipstricks.md", "Tips & Tricks"),    
     
-    Page("blocks.mdtext", "Blocks"),
-    Page("processors.mdtext", "Processors"),
+    Page("blocks.md", "Blocks"),
+    Page("processors.md", "Processors"),
     PythonPage("../cubictemp.py", 
         title="Source"),
-    Page("subs.mdtext","Tags, Expressions"),
-    Page("admin.mdtext", "Administrivia")
+    Page("subs.md","Tags, Expressions"),
+    Page("admin.md", "Administrivia")
 ]
